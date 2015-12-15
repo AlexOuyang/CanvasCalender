625d00d1de314b9d2971674cbc62128704d82f85
AlexOuyang <ouyangchenxing@hotmail.com>
2015-12-15 02:11:18 -0800

    calender resize done

--- js/calender.js
+++ js/calender.js
@@ -1,95 +1,134 @@
- //var canvas = document.getElementById('myCanvas');
- //canvas.width = window.innerWidth;
- //canvas.height = window.innerHeight;
- //var context = canvas.getContext('2d');
- //
- //var calender = function (context, beginingHour, endingHour, step) {
- //    this.context = context;
- //    this.beginingHour = beginingHour;
- //    this.endingHour = endingHour;
- //    this.step = step;
- //
- //    this.addClass = function (x, y, width, height, color) {
- //        this.context.beginPath();
- //        this.context.rect(x, y, width, height); // rect(x,y,width, height)
- //        this.context.fillStyle = color;
- //        this.context.fill();
- //        //this.context.lineWidth = 7;
- //        //this.context.strokeStyle = 'black';   // Add a sexy outline
- //        this.context.stroke();
- //    }
- //}
- //
- //
- //
- //var calender = new calender(context, 8, 20, 20);
- //calender.addClass(100, 100, 100, 100, 'green');
+//var canvas = document.getElementById('myCanvas');
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
+//var context = canvas.getContext('2d');
+//
+//var calender = function (context, beginingHour, endingHour, step) {
+//    this.context = context;
+//    this.beginingHour = beginingHour;
+//    this.endingHour = endingHour;
+//    this.step = step;
+//
+//    this.addClass = function (x, y, width, height, color) {
+//        this.context.beginPath();
+//        this.context.rect(x, y, width, height); // rect(x,y,width, height)
+//        this.context.fillStyle = color;
+//        this.context.fill();
+//        //this.context.lineWidth = 7;
+//        //this.context.strokeStyle = 'black';   // Add a sexy outline
+//        this.context.stroke();
+//    }
+//}
+//
+//
+//
+//var calender = new calender(context, 8, 20, 20);
+//calender.addClass(100, 100, 100, 100, 'green');
 
 
 
 
 
 
- function createCalender(w_scale, h_scale) {
-     var canvas = document.getElementById('myCanvas');
-     var context = canvas.getContext('2d');
-     canvas.width = window.innerWidth / w_scale;
-     canvas.height = window.innerHeight / h_scale;
+function createCalender(w_scale, h_scale) {
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    canvas.width = window.innerWidth / w_scale;
+    canvas.height = window.innerHeight / h_scale;
 
-     var calender = function (canvas, context, dayBeginingTime, dayEndingTime) {
-         var height = canvas.height;
-         var width = canvas.width;
-         var _week = {
-             "Monday": 1,
-             "Tuesday": 2,
-             "Wednesday": 3,
-             "Thursday": 4,
-             "Friday": 5,
-         };
-         var daysPerWeek = 5;
-         var _classWidth = width / daysPerWeek;
-         var _hoursPerDay = dayBeginingTime - dayEndingTime;
+    var calender = function (canvas, context, dayBeginingTime, dayEndingTime) {
+        var calender = this;
+        this.yOffset = 50;
+        this.xOffset = 50;
+        var _calenderHeight = canvas.height - this.yOffset;
+        var _calenderWidth = canvas.width - this.xOffset;
+        var _week = {
+            "Monday": 0,
+            "Tuesday": 1,
+            "Wednesday": 2,
+            "Thursday": 3,
+            "Friday": 4,
+        };
+        var _weekName = {
+            0: "Monday",
+            1: "Tuesday",
+            2: "Wednesday",
+            3: "Thursday",
+            4: "Friday"
+        };
+        var _daysPerWeek = 5;
+        var _dayWidth = _calenderWidth / _daysPerWeek;
+        var _hoursPerDay = dayEndingTime - dayBeginingTime;
+        if (_hoursPerDay < 0) console.error("'dayEndingTime' can not be smaller than 'dayBegininTime'")
+        var _hourHeight = _calenderHeight / _hoursPerDay;
 
-         this.addClass = function (className, dayOfTheWeek, classStartTime, classEndingTime, x, y, height, color) {
-             //             var _classHeight =
-             context.beginPath();
-             context.rect(x, y, _classWidth, height); // rect(x,y,width, height)
-             context.fillStyle = color;
-             context.fill();
-             context.lineWidth = 2;
-             context.strokeStyle = 'black'; // Add a sexy outline
-             context.stroke();
-         }
+        this.addClass = function (className, dayOfTheWeek, classStartTime, classEndingTime, classType, color) {
+            var class_x = (_week[dayOfTheWeek] * _dayWidth) + calender.xOffset;
+            var class_y = calender.yOffset + (classStartTime - dayBeginingTime) * _hourHeight;
+            var class_width = _dayWidth;
+            var class_height = (classEndingTime - classStartTime) * _hourHeight;
 
-         function drawLine(x_start, y_start, x_end, y_end) {
-             context.beginPath();
-             context.moveTo(x_start, y_start);
-             context.lineTo(x_end, y_end);
-             context.stroke();
-         }
+            context.beginPath();
+            context.rect(class_x, class_y, class_width, class_height); // rect(x,y,width, height)
+            context.fillStyle = color;
+            context.fill();
+            context.lineWidth = 2;
+            context.strokeStyle = "black"; // Add a sexy outline
+            context.stroke();
 
-         this.drawGrid = (function () {
-             drawLine(0, 20, width, 20);
-         }());
-     }
+            context.fillStyle = "black";
+            context.font = "15px Arial";
+            context.fillText(className +"  "+ classType, class_x + class_width / 4, class_y + class_height / 2);
+
+        }
+
+        function drawLine(x_start, y_start, x_end, y_end) {
+            context.beginPath();
+            context.moveTo(x_start, y_start);
+            context.lineTo(x_end, y_end);
+            context.stroke();
+        }
+
+        this.drawGrid = (function () {
+            // Draw horizontal grid
+            context.font = "15px Arial";
+            for (var i = 0; i <= _hoursPerDay; i++) {
+                var gridYPosition = (i * _hourHeight) + calender.yOffset;
+                drawLine(calender.xOffset, gridYPosition, _calenderWidth + calender.xOffset, gridYPosition);
+                context.fillText(i + dayBeginingTime, 0, gridYPosition);
+            }
+
+            context.font = "30px Arial";
+            // Draw vertical grid
+            for (var i = 0; i <= _daysPerWeek; i++) {
+                var gridXPosition = (i * _dayWidth) + calender.xOffset;
+                drawLine(gridXPosition, calender.yOffset, gridXPosition, _calenderHeight + calender.yOffset);
+                context.fillText(_weekName[i], gridXPosition + 40, calender.yOffset - 10);
+            }
+
+        }());
+    }
 
 
-     // resize the canvas to fill browser window dynamically
-     window.addEventListener('resize', resizeCanvas, false);
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
 
-     function resizeCanvas() {
-         canvas.width = window.innerWidth / w_scale;
-         canvas.height = window.innerHeight / h_scale;
+    function resizeCanvas() {
+        canvas.width = window.innerWidth / w_scale;
+        canvas.height = window.innerHeight / h_scale;
 
-         drawCalender();
-     }
-     resizeCanvas();
+        drawCalender();
+    }
+    resizeCanvas();
 
-     function drawCalender() {
-         var c = new calender(canvas, context, 8, 20);
-         c.addClass("cse 101", "Monday", 10, 10, 11, 13, 100, "green");
-         console.log("recreated calender");
-     }
- }
-
- //var class1 = ["cse 101", "Monday", 11, 2, "LE"]
+    function drawCalender() {
+        var c = new calender(canvas, context, 8, 20);
+        // addClass apram: (className, dayOfTheWeek, classStartTime, classEndingTime, classType, color)
+        c.addClass("cse 101", "Monday", 10, 12, "LE", "green"); //["cse 101 ", "Monday", 10, 12, "LE "]
+        c.addClass("cse 101", "Thursday", 14, 15.5, "LE", "green"); //["cse 101 ", "Thursday", 14, 15.5, "LE "]
+        c.addClass("cse 140", "Friday", 18, 20, "LE", "green"); //["cse 140 ", "Friday", 18, 20, "LE "]
+        c.addClass("cse 120", "Wednesday", 16, 18, "LE", "green"); //["cse 120 ", "Wednesday", 16, 18, "LE "]
+        c.addClass("cse 140", "Friday", 14, 16, "LE", "green"); //["cse 140 ", "Friday", 14, 16, "LE "]
+        console.log("recreated calender");
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,53 +1,63 @@
-var schedule = [ {'name': 'CSE103',
-		'links' : [
-			{
-				'title': 'Class Website',
-				'url': 'http://cse103.github.io'
+var schedule = [{
+        'name': 'CSE103',
+        'links': [
+            {
+                'title': 'Class Website',
+                'url': 'http://cse103.github.io'
 			},
-			{
-				'title': 'WebWork',
-				'url': 'http://webwork.cse.ucsd.edu/webwork2/CSE103_Fall2015'
+            {
+                'title': 'WebWork',
+                'url': 'http://webwork.cse.ucsd.edu/webwork2/CSE103_Fall2015'
 			},
-			{
-				'title': 'Piazza',
-				'url': 'http://piazza.com'
+            {
+                'title': 'Piazza',
+                'url': 'http://piazza.com'
 			},
-			{
-				'title': 'Textbopk',
-				'url': 'http://nb.mit.edu'
+            {
+                'title': 'Textbopk',
+                'url': 'http://nb.mit.edu'
 			}
 		],
-		'schedule' : [
-			{
-				'time': new Date(2015, 11, 8),
-				'activity': 'Final Exam'
+        'schedule': [
+            {
+                'time': new Date(2015, 11, 8),
+                'activity': 'Final Exam'
 			}
 		]
 	},
-	{
-		'name': 'CSE120',
-		'links' : [
-			{
-				'title': 'Class Website',
-				'url': 'http://cseweb.ucsd.edu/classes/fa15/cse120-b/'
+    {
+        'name': 'CSE120',
+        'links': [
+            {
+                'title': 'Class Website',
+                'url': 'http://cseweb.ucsd.edu/classes/fa15/cse120-b/'
 			},
-			{
-				"title": "Free Book",
-				"url": "http://pages.cs.wisc.edu/~remzi/OSTEP/"
+            {
+                "title": "Free Book",
+                "url": "http://pages.cs.wisc.edu/~remzi/OSTEP/"
 			}
 		],
-		'schedule' : [
-			{ 'time': new Date(2015, 9, 29), 'activity': 'Midterm Exam' },
-			{ 'time': new Date(2015, 11, 9), 'activity': 'Final Exam' },
-			{ 'time': new Date(2015, 9, 5), 'activity': 'Project #0' }
+        'schedule': [
+            {
+                'time': new Date(2015, 9, 29),
+                'activity': 'Midterm Exam'
+            },
+            {
+                'time': new Date(2015, 11, 9),
+                'activity': 'Final Exam'
+            },
+            {
+                'time': new Date(2015, 9, 5),
+                'activity': 'Project #0'
+            }
 		]
     }
  ];
- 
 
- //var canvas = document.getElementById('myCanvas');
+
+//var canvas = document.getElementById('myCanvas');
 //canvas.width = window.innerWidth;
 //canvas.height = window.innerHeight;
 //var context = canvas.getContext('2d');
 
-var calender = createCalender(2,2);
+var calender = createCalender(1.2, 1.2);
